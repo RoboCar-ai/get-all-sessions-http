ad80b0463b023bbe2b45e0e9d749927ce2c1cd15
Thomas Milas <itpro41994@gmail.com>
2018-08-03 09:11:16 -0700

    updated function to list folders in data directory.

--- index.js
+++ index.js
@@ -5,13 +5,26 @@
  * @param {!Object} res HTTP response context.
  */
 const bucket = require('@google-cloud/storage')().bucket('sacred-reality-201417-mlengine');
+const path = require('path');
 
 const auth = process.env.AUTH;
 
 exports.handler = (req, res) => {
-  if (req.headers.Authorization !== auth) return res.status(401).send();
-  
-  bucket.getFiles().then(list => res.json(list)).catch(err => res.status(500).json(err));
+  if (req.method === `OPTIONS`) {
+    return res.set('Access-Control-Allow-Origin', "*")
+       .set('Access-Control-Allow-Methods', 'GET, POST')
+       .status(200);
+  }
+  if (req.headers.authorization !== auth) return res.status(401).send();
 
-  res.status(200).send(message);
+  bucket.getFiles({
+    autoPaginate: false,
+    delimiter: "/",
+    prefix: "data/"
+  })
+    .then(data => res.json(data[2].prefixes.map(pre => path.basename(pre))))
+    .catch(err => {
+      console.log('error listing files', err);
+      res.status(500).json(err)
+    });
 };
